1ea7a28f8fbf1f12e8fab3372a93f45058a2bbd1
Matthew Dawson <mattdawson@ucar.edu>
2020-09-10 12:17:33 -0700

    update staging script for new folder names

--- stage_tag.py
+++ stage_tag.py
@@ -12,7 +12,7 @@ import argparse
 #default_preprocessor_server = "www.acom.ucar.edu"
 
 parser = argparse.ArgumentParser(
-                      description='Distribute Fortran from a particular directory into MICM_chemistry',
+                      description='Distribute Fortran from a particular directory into the MICM source folder',
                       formatter_class=argparse.ArgumentDefaultsHelpFormatter
                       )
 
@@ -20,8 +20,8 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-source_dir_kinetics', type=str, required=True,
                     help='Source directory containing preprocessed kinetics FORTRAN code and mechanism json')
 
-parser.add_argument('-target_dir_chemistry', type=str, default="../MICM_chemistry/src",
-                    help='Chemistry module source code folder. Kinetics code will be placed in the target_dir_chemistry\kinetics folder')
+parser.add_argument('-target_dir_chemistry', type=str, default="../micm/src/preprocessor_output/",
+                    help='Chemistry module source code folder for preprocessor output. Kinetics code will be placed here.')
 
 parser.add_argument('-source_file_environmental_conditions', type=str, default="./environmental_conditions/boulder.complete.nc",
                     help='Path to the environmental conditions NetCDF file.')
@@ -34,8 +34,8 @@ parser.add_argument('-target_dir_data', type=str, default="../../build/data/",
 args = parser.parse_args()
 print(args)
 
-cp( args.source_dir_kinetics + "/rate_constants_utility.F90", args.target_dir_chemistry  + "/preprocessor_output/rate_constants_utility.F90" )
-cp( args.source_dir_kinetics + "/factor_solve_utilities.F90", args.target_dir_chemistry  + "/preprocessor_output/factor_solve_utilities.F90" )
-cp( args.source_dir_kinetics + "/kinetics_utilities.F90"    , args.target_dir_chemistry  + "/preprocessor_output/kinetics_utilities.F90" )
+cp( args.source_dir_kinetics + "/rate_constants_utility.F90", args.target_dir_chemistry  + "rate_constants_utility.F90" )
+cp( args.source_dir_kinetics + "/factor_solve_utilities.F90", args.target_dir_chemistry  + "factor_solve_utilities.F90" )
+cp( args.source_dir_kinetics + "/kinetics_utilities.F90"    , args.target_dir_chemistry  + "kinetics_utilities.F90" )
 cp( args.source_dir_kinetics + "/source_mechanism.json"     , args.target_dir_data       + "molec_info.json" )
 cp( args.source_file_environmental_conditions,                args.target_dir_data       + "env_conditions.nc" )
