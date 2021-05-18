8330439531d2dbca295c8a4a3977b6222519dace
Matthew Dawson <mattdawson@ucar.edu>
2021-05-18 16:15:37 -0700

    rough switch to new format

--- preprocess_tag.py
+++ preprocess_tag.py
@@ -5,28 +5,12 @@
 
 import argparse
 import os
+from os.path import abspath
 import sys
 from ftplib import FTP
-
 from http.client import HTTPSConnection
 import json
-
 import requests
-
-headers = {
-        "cache-control": "no-cache",
-        "x-dreamfactory-api-key": "YOUR_API_KEY"
-}
-
-userAndPass = bytes('username' + ':' + 'password', "utf-8") #convert to utf-8
-
-
-# needs to check that ssl is enabled
-#import socket
-#socket.ssl
-# expect something like<function ssl at 0x4038b0>
-
-# needs to check the python v3 is running
 
 # Parse arguments.  They override those defaults specified in the argument parser
 default_preprocessor_server = "www.acom.ucar.edu"
@@ -34,73 +18,42 @@ parser = argparse.ArgumentParser(
                     description='Solve a mechanism tag using the sparse solver branch of MusicBox/MICM.',
                     formatter_class=argparse.ArgumentDefaultsHelpFormatter
                     )
-parser.add_argument('-mechanism_source_path', type=str, required=True,
-                    help='path to mechanism store location of tag')
-parser.add_argument('-preprocessor', type=str, default=default_preprocessor_server,
+parser.add_argument('-c','--configuration-file-path', type=abspath, default=abspath("config.json"),
+                    help='path to the mechanism configuration file')
+parser.add_argument('-p','--preprocessor', type=str, default=default_preprocessor_server,
                     help='url of preprocessor')
-parser.add_argument('-overwrite', type=bool, default=False,
-                    help='overwrite the target_dir')
- 
-
-
 args = parser.parse_args()
-
-mechanism_source_path = args.mechanism_source_path+"/"
-
-with open(mechanism_source_path+'this_configuration', 'w') as configuration_filehandle:
-  configuration_filehandle.write(str(args))
 
 # Connection to the preprocessor location
 processor_location = args.preprocessor
 preprocessor_con = HTTPSConnection(processor_location, 3000)
 
-# check connection status:
-#exception http.client.HTTPException
-#  The base class of the other exceptions in this module. It is a subclass of Exception.
-#exception http.client.NotConnected
-#  A subclass of HTTPException.
-
-#
-with open(mechanism_source_path+'source_mechanism.json', 'r') as json_mechanism_file:
-  mech_json = json.load(json_mechanism_file)
+# combine mechanism configuration into single json object
+mechanism_json = json.loads('{}')
+configuration_folder = os.path.split(args.configuration_file_path)[0];
+with open(args.configuration_file_path, 'r') as configuration_file:
+  configuration_json = json.load(configuration_file)
+  for file_name in configuration_json['pmc-files']:
+      with open(os.path.join(configuration_folder, file_name), 'r') as mechanism_file:
+          mechanism_json[file_name] = json.load(mechanism_file)
 
 #
 #    Turn mechanism.json into fortran code!
 #
-# preprocessor headers
-headers = { 'Authorization' : 'Basic %s' %  userAndPass, 'Content-type': 'application/json', 'Accept': 'text/plain' }
-
-# Construct factor_solve_utilities.F90, kinetics_utilities.F90, rate_constants_utilities.F90
-#mech_json_string = json.dumps(mech_json)
-#print(mech_json_string)
-if 'mechanism_format' in mech_json["mechanism"]:
-  mechanism_format = mech_json["mechanism"]["mechanism_format"]
-  res = requests.post("http://"+args.preprocessor+"/constructJacobian/v"+mechanism_format, auth=('user', 'pass'), json=mech_json)
-else:
-  res = requests.post("http://"+args.preprocessor+"/constructJacobian/", auth=('user', 'pass'), json=mech_json)
-
-#print(res.status_code)
-#print(res.encoding)
-#print(res.json)
+# get preprocessed code
+res = requests.post("http://"+args.preprocessor+"/constructJacobian/", auth=('user', 'pass'), json=mechanism_json)
 if res.status_code != 200 : exit()
 jacobian = res.text
 jacobian_json = json.loads(jacobian)
-#print(jacobian_json)
 
-#service = '/preprocessor/constructJacobian'
-#preprocessor_con.request('POST', service, mechanism, headers=headers)
-#res = preprocessor_con.getresponse()
-#jacobian = res.read()  
-#jacobian_json = json.loads(jacobian)
-
-
-# write factor_solve_utilities.F90, kinetics_utilities.F90, rate_constants_utilities.F90
-with open(mechanism_source_path+'kinetics_utilities.F90', 'w') as k_file:
+# write preprocessed code to output folder
+output_folder = os.path.join(configuration_folder, 'output')
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+with open(os.path.join(output_folder, 'kinetics_utilities.F90'), 'w') as k_file:
   k_file.write(jacobian_json["kinetics_utilities_module"])
-
-with open(mechanism_source_path+'rate_constants_utility.F90', 'w') as r_file:
+with open(os.path.join(output_folder, 'rate_constants_utility.F90'), 'w') as r_file:
   r_file.write(jacobian_json["rate_constants_utility_module"])
-
-with open(mechanism_source_path+'factor_solve_utilities.F90', 'w') as f_file:
+with open(os.path.join(output_folder, 'factor_solve_utilities.F90'), 'w') as f_file:
   f_file.write(jacobian_json["factor_solve_utilities_module"])
 
