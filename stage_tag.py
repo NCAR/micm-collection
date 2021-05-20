5f6e4efc84f130383e214a4744cf2788341e11ae
Matthew Dawson <mattdawson@ucar.edu>
2021-05-20 12:23:20 -0700

    finish udpates for new mechanism json format

--- stage_tag.py
+++ stage_tag.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(
 
 
 parser.add_argument('-source_dir_kinetics', type=str, required=True,
-                    help='Source directory containing preprocessed kinetics FORTRAN code and mechanism json')
+                    help='Source directory containing preprocessed kinetics FORTRAN code')
 
 parser.add_argument('-target_dir_chemistry', type=str, default="../micm/src/preprocessor_output/",
                     help='Chemistry module source code folder for preprocessor output. Kinetics code will be placed here.')
@@ -37,5 +37,4 @@ print(args)
 cp( args.source_dir_kinetics + "/rate_constants_utility.F90", args.target_dir_chemistry  + "rate_constants_utility.F90" )
 cp( args.source_dir_kinetics + "/factor_solve_utilities.F90", args.target_dir_chemistry  + "factor_solve_utilities.F90" )
 cp( args.source_dir_kinetics + "/kinetics_utilities.F90"    , args.target_dir_chemistry  + "kinetics_utilities.F90" )
-cp( args.source_dir_kinetics + "/source_mechanism.json"     , args.target_dir_data       + "molec_info.json" )
 cp( args.source_file_environmental_conditions,                args.target_dir_data       + "env_conditions.nc" )
